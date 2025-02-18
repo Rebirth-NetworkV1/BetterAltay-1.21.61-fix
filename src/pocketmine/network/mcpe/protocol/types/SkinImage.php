5ee7b0150a5fe99a144bbd736601f46f99bbaf86
tr <twizyrichhtr@gmail.com>
2025-02-18 20:33:56 +0000

    Update SkinImage.php

--- src/pocketmine/network/mcpe/protocol/types/SkinImage.php
+++ src/pocketmine/network/mcpe/protocol/types/SkinImage.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
 declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;
@@ -27,54 +8,52 @@ use InvalidArgumentException;
 use function strlen;
 
 class SkinImage {
+	private int $height;
+	private int $width;
+	private string $data;
 
-	private $height;
-	private $width;
-	private $data;
+	public function __construct(int $height, int $width, string $data) {
+		$this->height = max(1, $height);
+		$this->width = max(1, $width);
 
-	public function __construct(int $height, int $width, $data) {
-		if (!is_string($data)) {
-			$data = "";
-		}
-		if ($height < 0 or $width < 0) {
-			throw new InvalidArgumentException("Height and width cannot be negative");
-		}
-		$expected = $height * $width * 4;
+		$expected = $this->height * $this->width * 4;
 		$actual = strlen($data);
-		if ($expected !== $actual) {
-			$data = "";
-		}
-		$this->height = $height;
-		$this->width = $width;
-		$this->data = $data;
-	}
 
-	public static function fromLegacy(string $data) : SkinImage {
-		switch (strlen($data)) {
-			case 64 * 32 * 4:
-				return new self(32, 64, $data);
-			case 64 * 64 * 4:
-				return new self(64, 64, $data);
-			case 128 * 128 * 4:
-				return new self(128, 128, $data);
-			case 256 * 128 * 4:
-				return new self(128, 256, $data);
-			case 256 * 256 * 4:
-				return new self(256, 256, $data);
-			default:
-				return new self(0, 0, "");
+		if ($actual !== $expected) {
+			$this->data = str_repeat("\x00", $expected); // Transparent blank skin
+		} else {
+			$this->data = $data;
 		}
 	}
 
-	public function getHeight() : int {
+	public static function fromLegacy(string $data): SkinImage {
+		$sizes = [
+			64 * 32 * 4 => [32, 64],
+			64 * 64 * 4 => [64, 64],
+			128 * 128 * 4 => [128, 128],
+			256 * 128 * 4 => [128, 256],
+			256 * 256 * 4 => [256, 256],
+		];
+
+		$size = strlen($data);
+		if (isset($sizes[$size])) {
+			[$height, $width] = $sizes[$size];
+			return new self($height, $width, $data);
+		}
+
+		// Default to a 64x64 blank skin if data is invalid
+		return new self(64, 64, str_repeat("\x00", 64 * 64 * 4));
+	}
+
+	public function getHeight(): int {
 		return $this->height;
 	}
 
-	public function getWidth() : int {
+	public function getWidth(): int {
 		return $this->width;
 	}
 
-	public function getData() : string {
+	public function getData(): string {
 		return $this->data;
 	}
 }
