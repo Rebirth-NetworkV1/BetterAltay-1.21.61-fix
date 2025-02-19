7824e02f441f9bbe66362ed0f773292ceffb0f30
tr <twizyrichhtr@gmail.com>
2025-02-19 20:01:05 +0000

    Update SkinImage.php

--- src/pocketmine/network/mcpe/protocol/types/SkinImage.php
+++ src/pocketmine/network/mcpe/protocol/types/SkinImage.php
@@ -6,38 +6,31 @@ namespace pocketmine\network\mcpe\protocol\types;
 
 use InvalidArgumentException;
 use function strlen;
-use function str_repeat;
 
 class SkinImage {
 
-    private int $height;
-    private int $width;
-    private string $data;
+    /** @var int */
+    private $height;
+    /** @var int */
+    private $width;
+    /** @var string */
+    private $data;
 
-    public function __construct(int $height, int $width, string $data) {
-        // If dimensions are invalid, substitute with a default 64x64 skin.
-        if ($height <= 0 || $width <= 0) {
-            $height = 64;
-            $width = 64;
-            $data = str_repeat("\x00", $height * $width * 4);
+    public function __construct(int $height, int $width, $data){
+        if($height < 0 or $width < 0){
+            $height = 0;
+            $width = 0;
+            $data = "";
         }
-        
-        $expectedSize = $height * $width * 4;
-        $actualSize = strlen($data);
-
-        // If the data size does not match the expected size, substitute with default skin.
-        if ($actualSize !== $expectedSize) {
-            $height = 64;
-            $width = 64;
-            $data = str_repeat("\x00", $height * $width * 4);
+        if(($expected = $height * $width * 4) !== ($actual = strlen($data))){
+            $data = "";
         }
-
         $this->height = $height;
         $this->width = $width;
         $this->data = $data;
     }
 
-    public static function fromLegacy(string $data): SkinImage {
+    public static function fromLegacy(string $data) : SkinImage{
         switch(strlen($data)){
             case 64 * 32 * 4:
                 return new self(32, 64, $data);
@@ -50,20 +43,20 @@ class SkinImage {
             case 256 * 256 * 4:
                 return new self(256, 256, $data);
             default:
-                // Fallback: return a valid 64x64 blank skin.
-                return new self(64, 64, str_repeat("\x00", 64 * 64 * 4));
+                return new self(0, 0, "");
         }
     }
 
-    public function getHeight(): int {
+    public function getHeight() : int{
         return $this->height;
     }
 
-    public function getWidth(): int {
+    public function getWidth() : int{
         return $this->width;
     }
 
-    public function getData(): string {
+    public function getData() : string{
         return $this->data;
     }
 }
+
